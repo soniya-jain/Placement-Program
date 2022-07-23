5f42cb103988d480149d8993e4d9cc26fd99b5ad
soniya-jain <92991101+soniya-jain@users.noreply.github.com>
2022-07-23 23:24:19 +0530

    Time: 10 ms (12.90%), Space: 7.1 MB (99.72%) - LeetHub

---
+++ 412-fizz-buzz/412-fizz-buzz.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string>ans(n);
+        for(int i=1; i<=n; i++)      
+            ans[i-1] = to_string(i);
+        for(int i=2; i<n; i+=3)
+            ans[i]= "Fizz";
+        for(int i=4; i<n; i+=5)
+            ans[i]= "Buzz";
+        for(int i=14; i<n; i+=15){
+            ans[i]= "FizzBuzz";
+        }
+        return ans; 
+    }
+};
