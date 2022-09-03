10709f8e90291e3ad025d64f5815c2e1a5590431
soniya-jain <92991101+soniya-jain@users.noreply.github.com>
2022-09-04 01:17:14 +0530

    Time: 3 ms (46.75%), Space: 6.3 MB (50.79%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     bool isValid(string s){
-       string st;
        unordered_map<char,char>mp;
        mp['('] = ')';
        mp['['] = ']';
        mp['{'] = '}';
-       st.push_back(s[0]);// st="("
-       for(int i=1; i<s.size(); i++){
-           if(mp[st.back()]==s[i])
-               st.pop_back();
-           else
-               st.push_back(s[i]);
+       string temp;
+       temp.push_back(s[0]);
+       for(int i = 1; i < s.size(); i++){
+           if(mp[temp.back()] == s[i]){
+               temp.pop_back();
+           }
+           else{
+               temp.push_back(s[i]);
+           }
        }
-       return st.size() ? 0 : 1;
+        int n = temp.size();
+        return n ? 0 : 1;
     }
 };
