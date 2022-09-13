3346f76ed31fbb1d941fe41b0f3fdd554ee40981
soniya-jain <92991101+soniya-jain@users.noreply.github.com>
2022-09-14 01:13:45 +0530

    Time: 83 ms (5.10%), Space: 20.2 MB (13.31%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -2,22 +2,20 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         // Hashmap Approach
-      /* unordered_map<int,int>mp;
-        int ans;
-        for(int i=0; i<nums.size(); i++) mp[nums[i]]++;
+        unordered_map<int,int>mp;
+        for(int i=0; i < nums.size(); i++)
+           mp[nums[i]]++;
         for(auto it: mp){
-            if(it.second==1){
-                ans=it.first;
-                return ans;
-            }
+            if(it.second == 1)
+                return it.first;
         }
-        return -1;*/
+        return -1;
         
-        // 2nd Efficient Approach --> Using XOR operator 
+        /* 2nd Efficient Approach --> Using XOR operator 
         int ans = 0;
         for(auto x: nums){
             ans ^= x;
         }
-        return ans;
+        return ans; */
     }
 };
