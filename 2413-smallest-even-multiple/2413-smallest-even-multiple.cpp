b272968630a31fee123c642dc7808af521a67137
soniya-jain <92991101+soniya-jain@users.noreply.github.com>
2022-09-28 17:24:23 +0530

    Time: 5 ms (15.29%), Space: 5.8 MB (70.69%) - LeetHub

---
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int smallestEvenMultiple(int n) {
+        int ans;
+            if(n%2 == 0)
+               ans = n;
+            else
+                ans = n*2;
+        return ans;
+    }
+};
